e7878d3bcfd77def6f57cf32e1d45b67541e9c41
sreeharika <sreeharikayelle@gmail.com>
2020-04-28 18:46:04 +0530

    added backend changes and added frontend build files

---
+++ frontend/build/precache-manifest.36aa21418c4c21c28e03750934221468.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ea9b88e7ab209cee35ccc7f890bccd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16a55e020edfcbfa8d2f",
+    "url": "/static/css/main.e9dded07.chunk.css"
+  },
+  {
+    "revision": "079f7654f3ca153c58d3",
+    "url": "/static/js/2.5483013e.chunk.js"
+  },
+  {
+    "revision": "d0550c944a9928585a31b6efc1a5d942",
+    "url": "/static/js/2.5483013e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16a55e020edfcbfa8d2f",
+    "url": "/static/js/main.8b6b775e.chunk.js"
+  },
+  {
+    "revision": "c8969de42819b4cc4b7c",
+    "url": "/static/js/runtime-main.45b5dce8.js"
+  }
+]);
